b827f80da026ae63bfa9a3edb723b9f21a458f11
Kevin Zhou <kevinzhou2222@gmail.com>
2020-06-11 20:54:54 -0400

    Enable Boids to follow mouse

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 const BOID_SIZE = 15.0;
-const MAX_SPEED = 5;
-const MAX_FORCE = 0.05;
+const MAX_SPEED = 2;
+const MAX_FORCE = 0.1;
 
 let boids;
 let longerLength;
@@ -62,7 +62,18 @@ class Boid {
     }
 
     update() {
-        this.position.add(this.velocity.limit(MAX_SPEED));
+        if(followCheck.checked()) {
+            let scaledMouseX = map(mouseX, 0, width, 0, 100);
+            let scaledMouseY = map(mouseY, 0, height, 0, 100);
+            this.acceleration.add(createVector(scaledMouseX - this.position.x, scaledMouseY - this.position.y).limit(1));
+        }
+
+        this.acceleration = this.acceleration.limit(MAX_FORCE);
+        this.velocity.add(this.acceleration);
+
+        this.velocity = this.velocity.limit(MAX_SPEED);
+        this.position.add(this.velocity);
+
         // Add 100 to prevent values from becoming negative, since JavaScript modulus doesn't limit to positive results
         this.position.x = (this.position.x+100) % 100;
         this.position.y = (this.position.y+100) % 100;
@@ -76,7 +87,7 @@ class Boid {
         let yCanvas = map(this.position.y, 0, 100, 0, height);
         translate(xCanvas, yCanvas);
         rotate(this.velocity.heading());
-        triangle(-BOID_SIZE, BOID_SIZE/3, -BOID_SIZE, -BOID_SIZE/3, 0, 0);
+        triangle(0, BOID_SIZE/3, 0, -BOID_SIZE/3, BOID_SIZE, 0);
         pop();
     }
 }
